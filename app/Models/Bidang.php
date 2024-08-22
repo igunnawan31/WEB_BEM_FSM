3f3c3584971efa0ccd300e82735083eb2f68918a
Muhamad Gunawan <igunnawan24@gmail.com>
2024-08-23 03:33:50 +0700

    perubahan terbaru (adding resources bidang

---
+++ app/Models/Bidang.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Bidang extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'bidang',
+        'slug',
+        'penjelasan',
+        'path_foto_bidang',
+    ];
+
+    public function bidangs() : HasMany {
+        return $this->hasMany(Anggota::class, 'bidang_id');
+    }
+}
---
+++ database/migrations/2024_08_22_185108_create_anggotas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('anggotas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->foreignId('bidang_id')->constrained(
+                table: 'bidangs',
+                indexName: 'bidangs_anggota_id'
+            );
+            $table->string('bidang');
+            $table->string('jabatan');
+            $table->string('path_foto_anggota');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('anggotas');
+    }
+};
