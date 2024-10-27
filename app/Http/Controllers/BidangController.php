c8f56a4f468d7126d27a3e6456a1e64217797517
Muhamad Gunawan <igunnawan24@gmail.com>
2024-10-28 01:50:27 +0700

    terbaru

--- app/Http/Controllers/BidangController.php
+++ app/Http/Controllers/BidangController.php
@@ -10,11 +10,6 @@ class BidangController extends Controller
     public function index() {
         // Retrieve all bidang records from the database
         $bidangs = Bidang::all();
-
-        // Pass the bidang records to the view
-<<<<<<< HEAD
-        return view('bidang', ['bidangs' => $bidangs]);
-=======
         return view('bidangs', compact('bidangs'));
     }
 
@@ -22,7 +17,6 @@ class BidangController extends Controller
     {
         $bidang = Bidang::where('bidang', $namabidang)->firstOrFail();
         return view('bidang', compact('bidang'));
->>>>>>> branch_igun
     }
 }
 
