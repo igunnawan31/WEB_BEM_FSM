c258baadb435a958b54d5db805f956bc6ce7734c
DaffaHirza <3103120061@student.smktelkom-pwt.sch.id>
2024-10-27 20:41:09 +0700

    update terbaru

---
+++ app/Filament/Resources/BidangResource.php
@@ -0,0 +1,83 @@
+<<<<<<< HEAD
+=======
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BidangResource\Pages;
+use App\Filament\Resources\BidangResource\RelationManagers;
+use App\Models\Bidang;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BidangResource extends Resource
+{
+    protected static ?string $model = Bidang::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('bidang')->label('Nama Bidang')->required(),
+                Forms\Components\TextInput::make('slug')->label('Slug')->required(),
+                Forms\Components\TextInput::make('penjelasan')->label('Deskripsi Singkat')->required(),
+                Forms\Components\TextInput::make('path_foto_bidang')->label('Path Foto Bidang')->required(),
+                Forms\Components\TextInput::make('path_logo_bidang')->label('Path Logo Bidang')->required(),
+                // $table->String('Bidang');
+                // $table->String('slug')->unique();
+                // $table->text('Penjelasan');
+                // $table->String('Path_Foto_Bidang');
+                // $table->timestamps();
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('bidang')->label('Nama Bidang'),
+                Tables\Columns\TextColumn::make('slug')->label('Slug'),
+                Tables\Columns\TextColumn::make('penjelasan')->label('Deskripsi Singkat'),
+                Tables\Columns\ImageColumn::make('path_foto_bidang')->label('Path Foto Bidang'),
+                Tables\Columns\ImageColumn::make('path_logo_bidang')->label('Path Logo Bidang'),
+                
+                // Tables\Columns\TextColumn::make('created_at')->label('Created At'),
+                // Tables\Columns\TextColumn::make('updated_at')->label('Updated At'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBidangs::route('/'),
+            'create' => Pages\CreateBidang::route('/create'),
+            'edit' => Pages\EditBidang::route('/{record}/edit'),
+        ];
+    }
+}
+>>>>>>> parent of 426ad41 (update post ig dan raphin)
